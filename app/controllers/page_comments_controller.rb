65141d2b5f806c43b383af5c7588ac43a8ac4eca
peakpg <peakpg@gmail.com>
2009-04-29 17:22:56 -0400

    Fixed #44. Users will need to log in to post comments.

--- app/controllers/page_comments_controller.rb
+++ app/controllers/page_comments_controller.rb
@@ -1,4 +1,7 @@
-class PageCommentsController < ApplicationController
+class PageCommentsController < Cms::ApplicationController
+
+  before_filter :verify_user_is_logged_in
+
   def create
     @page_comment = PageComment.new(params[:page_comment])
     @page_comment.ip = request.remote_ip
@@ -6,5 +9,11 @@ class PageCommentsController < ApplicationController
       flash[:notice] = "Comment was created"
     end
     redirect_to @page_comment.page.path
+  end
+
+  private
+  
+  def verify_user_is_logged_in
+    raise Cms::Errors::AccessDenied if current_user.guest?
   end
 end
